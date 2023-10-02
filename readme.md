353f3c704fd85ba3ec79be215a13834a223b4440
Oroxhimaru <hm51562@gmail.com>
2023-10-03 00:53:17 +0500

    Day 2

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 30 Days of JavaScript (Leetcode)
+1. This is a solution of the 30 days JavaScript program of Leetcode. Here is the link https://leetcode.com/studyplan/30-days-of-javascript/ .
+2. if you don't understand any solution feel free to ask in comments. or you can reach me out on my email : hm51562@gmail.com . 
--- script30days.js
+++ script30days.js
@@ -1,13 +1,30 @@
 /*  Write a function createHelloWorld. It should return a new function that always returns "Hello World" */
 
 function createHelloWorld() {
-    return function() {
-        return "Hello World";
-    };
+  return function () {
+    return "Hello World";
+  };
 }
 
 // Usage
 const helloWorldFunction = createHelloWorld();
 console.log(helloWorldFunction()); // Output: "Hello World"
 
+/* Given an integer n, return a counter function.
+ This counter function initially returns n and then returns 1 more than the 
+ previous value every subsequent time it is called (n, n + 1, n + 2, etc). */
+
+
+ function createCounter(n) {
+    return function() {
+        return n++;
+    };
+}
+
+// Usage
+const counter = createCounter(10); // Initialize the counter with the value 10
+
+console.log(counter()); // Output: 10
+console.log(counter()); // Output: 11
+console.log(counter()); // Output: 12
 
