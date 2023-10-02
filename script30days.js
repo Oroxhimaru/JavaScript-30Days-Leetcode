6f100283a82968177240f04c95540482169c2c1c
Oroxhimaru <hm51562@gmail.com>
2023-10-03 01:14:43 +0500

    Day 3

--- script30days.js
+++ script30days.js
@@ -28,3 +28,35 @@ console.log(counter()); // Output: 10
 console.log(counter()); // Output: 11
 console.log(counter()); // Output: 12
 
+
+/* Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+
+toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal". */
+
+function expect(val) {
+  return {
+      toBe: function(expectedValue) {
+          if (val !== expectedValue) {
+              throw new Error("Not Equal");
+          }
+          return true;
+      },
+      notToBe: function(unexpectedValue) {
+          if (val === unexpectedValue) {
+              throw new Error("Equal");
+          }
+          return true;
+      }
+  };
+}
+
+// Example usage
+try {
+  expect(5).toBe(5); // This will not throw an error
+  expect(5).notToBe(10); // This will not throw an error
+  expect(5).toBe(10); // This will throw an error "Not Equal"
+  expect(5).notToBe(5); // This will throw an error "Equal"
+} catch (error) {
+  console.error(error.message);
+}
